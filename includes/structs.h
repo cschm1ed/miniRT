2b50d1c99d23acfc9b778cf9228540279c4b85d9
EikoFresh <109620716+EikoFresh@users.noreply.github.com>
2023-06-18 21:16:24 +0200

    finch

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: estruckm <estruckm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 16:48:32 by estruckm          #+#    #+#             */
-/*   Updated: 2023/06/18 19:51:04 by estruckm         ###   ########.fr       */
+/*   Updated: 2023/06/18 21:02:51 by estruckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,70 @@ typedef struct s_pos
 	float	z;
 } t_pos;
 
+typedef struct s_vector
+{
+	float	x;
+	float	y;
+	float	z;
+} t_vector;
+
 typedef struct s_parsing
 {
 	char	*name;
 	int		(*f)(char **str);
 } t_parsing;
+
+typedef struct s_sphere
+{
+	t_pos	*center;
+	int		diameter;
+	int		colour;
+} t_sphere;
+
+typedef struct s_plane
+{
+	t_pos		*point;
+	t_vector	*vector;
+	int colour;
+} t_plane;
+
+typedef struct s_cylinder
+{
+	t_pos *center;
+	t_vector *vector;
+	int diameter;
+	int height;
+	int colour;
+} t_cylinder;
+
+typedef camera
+{
+	t_pos *center;
+	t_vector *s_vector;
+	int degrees;
+} t_camera;
+
+typedef ambient_light
+{
+	float light_ratio;
+	int colour;
+} t_ambient_light;
+
+typedef light_source
+{
+	t_pos *center;
+	float light_ratio;
+	int colour;
+} t_light_source;
+
+typedef scene
+{
+	t_camera *camera;
+	t_light_source *light_lst;
+	t_ambient_light *ambient_light;
+	t_sphere *sphere_lst;
+	t_plane *plane_lst;
+	t_cylinder *cylinder_lst;
+} t_scene;
 
 #endif
--- sources/utils/string_utils1.c
+++ sources/utils/string_utils1.c
@@ -4,3 +4,17 @@ int ft_strcmp(char *s1, char *s2)
 {
 	return (ft_strncmp(s1, s2, ft_strlen(s1) + 1));
 }
+
+long double    ft_atod(const char *str)
+{
+	long double	result;
+	long double	float_part;
+
+	result = ft_atoi(str);
+	while (*str != '.' && *str != ',' && *str != '\0')
+		str++;
+	float_part = ft_atoi(str + 1);
+	while (float_part >= 1)
+		float_part /= 10;
+	return (result + float_part);
+}
