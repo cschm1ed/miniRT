839977a651314b3291509bc881dff067915424bf
EikoFresh <109620716+EikoFresh@users.noreply.github.com>
2023-06-29 02:36:19 +0200

    cylinder

--- sources/intersections/line_cylinder.c
+++ sources/intersections/line_cylinder.c
@@ -12,7 +12,20 @@
 
 #include "../../includes/minirt.h"
 
-int line_cylinder(t_cylinder cylinder, t_line line)
+int line_cylinder(t_cylinder cylinder, t_line line, t_vector *result)
 {
+	t_vector t;
+	t_vector y;
+	t_vector x;
+
+
+	t = dot(subtract(cylinder.center, line.base), line.direction);
+	y = vector_len(subtract(cylinder.center, line_base));
+	if (y > cylinder.diameter / 2)
+		return (FALSE);
+	x =
+
+
+
 
 }
