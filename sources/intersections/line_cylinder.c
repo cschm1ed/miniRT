25c13b97e91701f541dddf28af12601c47427877
EikoFresh <109620716+EikoFresh@users.noreply.github.com>
2023-07-06 15:48:15 +0200

    neuer stuff

--- sources/intersections/line_cylinder.c
+++ sources/intersections/line_cylinder.c
@@ -12,89 +12,120 @@
 
 #include "../../includes/minirt.h"
 
-//static int line_plane_intersection(t_line line, t_cylinder cylinder, t_vector *result)
+//int line_cylinder(void *object, t_line line, t_vector *result)
 //{
-//	double dot_Product;
-//	double distance;
+//	t_vector h;
+//	t_vector apex;
+//	t_vector h_normal;
+//	double A;
+//	double B;
+//	double C;
 //
-//	dot_Product = _dot(line.direction, cylinder.axis_direction);
-//	if (dot_Product == 0)
-//		return (FALSE);
-//	distance = _dot(_subtract(cylinder.center, line.base), cylinder.axis_direction) / dot_Product;
-//	if (distance < 0)
-//		return (FALSE);
-//	*result = _add(line.base, _multiply(line.direction, distance));
-//	return (TRUE);
-//	//intersectionPoint = rayOrigin + distance * rayDirection
-//}
+//	double t_1;
+//	double t_2;
+//	t_cylinder cylinder;
 //
-//static int check_direction_intersection(t_vector intersect, t_vector apex, t_cylinder cylinder)
-//{
-//	double len_max;
-//	double len_intersect;
+//	cylinder = *((t_cylinder*)object);
+//	line.direction = _divide(line.direction, _len(line.direction));
+//	cylinder.axis_direction = _divide(cylinder.axis_direction, _len(cylinder.axis_direction));
 //
-//	len_max = sqrt(pow(cylinder.diameter / 2, 2) + pow(_len(_subtract(cylinder.center, apex)), 2));
-//	len_intersect = _len(_subtract(apex, intersect));
-//	if (len_intersect <= len_max)
+//	apex = _add(cylinder.center, _multiply(cylinder.axis_direction, cylinder.height));
+//	h = _subtract(apex, cylinder.center);
+//	h_normal = _divide(h, _len(h));
+//	/*h=(H−C)ĥ =(H−C)‖H−C‖*/
+//
+////	A = ||V||^2 * ||D||^2
+////	B = 2((V dot D)(P - O) dot D - (V dot D)^2)
+////	C = ||P - O||^2 * ||D||^2 - ((P - O) dot D)^2 - r^2
+////
+////	At^2 + Bt + C = 0 what the equation to solve for t
+////
+////			ChatGPT
+////	To solve the quadratic equation At^2 + Bt + C = 0, you can use the quadratic formula:
+////
+////	t = (-B ± √(B^2 - 4AC)) / (2A)
+//
+//	A = pow(_len(line.direction), 2);
+//	A *= pow(_len(h_normal), 2);
+//	B = 2 * (_dot(line.direction, cylinder.axis_direction) * _dot(_subtract(line.base, cylinder.center), h_normal) - pow(_dot(line.direction, h_normal), 2));
+//	C = pow(_len(_subtract(line.base, cylinder.center)), 2);
+//	C *= pow(_len(h_normal), 2) - pow(_dot(_subtract(line.base, cylinder.center), h_normal), 2);
+//	C -= pow(cylinder.diameter / 2, 2);
+//
+//	t_1 = (-1) * B + sqrt(pow(B, 2) - 4 * A * C);
+//	t_1 /= 2 * A;
+//	if (t_1 > 0)
+//	{
+//		*result = _add(line.base, _multiply(line.direction, t_1));
 //		return (TRUE);
+//	}
+//
+//	t_2 = (-1) * B - sqrt((pow(B, 2) - 4 * A * C) / 2 * A);
+//	if (t_2 > 0)
+//	{
+//		*result = _add(line.base, _multiply(line.direction, t_2));
+//		return (TRUE);
+//	}
 //	return (FALSE);
 //}
 int line_cylinder(void *object, t_line line, t_vector *result)
 {
-	t_vector h;
-	t_vector apex;
-	t_vector h_normal;
-	double A;
-	double B;
-	double C;
-
-	double t_1;
-	double t_2;
+	double a;
+	double b;
+	double c;
+	double delta;
+	double t1;
+	double t2;
+	double t;
+	double r;
 	t_cylinder cylinder;
 
 	cylinder = *((t_cylinder*)object);
-	line.direction = _divide(line.direction, _len(line.direction));
-	cylinder.axis_direction = _divide(cylinder.axis_direction, _len(cylinder.axis_direction));
 
-	apex = _add(cylinder.center, _multiply(cylinder.axis_direction, cylinder.height));
-	h = _subtract(apex, cylinder.center);
-	h_normal = _divide(h, _len(h));
-	/*h=(H−C)ĥ =(H−C)‖H−C‖*/
 
-//	A = ||V||^2 * ||D||^2
-//	B = 2((V dot D)(P - O) dot D - (V dot D)^2)
-//	C = ||P - O||^2 * ||D||^2 - ((P - O) dot D)^2 - r^2
-//
-//	At^2 + Bt + C = 0 what the equation to solve for t
-//
-//			ChatGPT
-//	To solve the quadratic equation At^2 + Bt + C = 0, you can use the quadratic formula:
-//
-//	t = (-B ± √(B^2 - 4AC)) / (2A)
-
-	A = pow(_len(line.direction), 2);
-	A *= pow(_len(h_normal), 2);
-	B = 2 * (_dot(line.direction, cylinder.axis_direction) * _dot(_subtract(line.base, cylinder.center), h_normal) - pow(_dot(line.direction, h_normal), 2));
-	C = pow(_len(_subtract(line.base, cylinder.center)), 2);
-	C *= pow(_len(h_normal), 2) - pow(_dot(_subtract(line.base, cylinder.center), h_normal), 2);
-	C -= pow(cylinder.diameter / 2, 2);
-
-	t_1 = (-1) * B + sqrt(pow(B, 2) - 4 * A * C);
-	t_1 /= 2 * A;
-	if (t_1 > 0)
-	{
-		*result = _add(line.base, _multiply(line.direction, t_1));
-		return (TRUE);
-	}
-
-	t_2 = (-1) * B - sqrt((pow(B, 2) - 4 * A * C) / 2 * A);
-	if (t_2 > 0)
-	{
-		*result = _add(line.base, _multiply(line.direction, t_2));
-		return (TRUE);
-	}
-	return (FALSE);
+	a = (line.direction.x * line.direction.x) + (line.direction.z * line.direction.z);
+	b = 2 * (line.direction.x * (line.base.x - cylinder.center.x) + line.direction.z * (line.base.z - cylinder.center.z));
+	c = (line.base.x - cylinder.center.x) * (line.base.x -cylinder.center.x) + (line.base.z - cylinder.center.z) * (line.base.z - cylinder.center.z) - (cylinder.diameter / 2 * cylinder.diameter / 2);
+	delta = b * b - 4 * (a * c);
+	if (fabs(delta) < 0.001)
+		return (FALSE);
+	if (delta < 0)
+		return (FALSE);
+	t1 = (-b - sqrt(delta)) / (2 * a);
+	t2 = (-b + sqrt(delta)) / (2 * a);
+	if (t1 > t2)
+		t = t2;
+	else
+		t = t1;
+	r = line.base.y + t * line.direction.y;
+	if ((r >= cylinder.center.y) && (r <= cylinder.center.y + cylinder.height))
+		return (FALSE);
+	*result = _add(line.base, _multiply(line.direction, t));
+	return (TRUE);
 }
+
+//float Cylinder::intersect(Vector pos, Vector dir)
+//{
+//	float a = (dir.x * dir.x) + (dir.z * dir.z);
+//	float b = 2*(dir.x*(pos.x-center.x) + dir.z*(pos.z-center.z));
+//	float c = (pos.x - center.x) * (pos.x - center.x) + (pos.z - center.z) * (pos.z - center.z) - (radius*radius);
+//
+//	float delta = b*b - 4*(a*c);
+//	if(fabs(delta) < 0.001) return -1.0;
+//	if(delta < 0.0) return -1.0;
+//
+//	float t1 = (-b - sqrt(delta))/(2*a);
+//	float t2 = (-b + sqrt(delta))/(2*a);
+//	float t;
+//
+//	if (t1>t2) t = t2;
+//	else t = t1;
+//
+//	float r = pos.y + t*dir.y;
+//
+//	if ((r >= center.y) and (r <= center.y + height))return t;
+//	else return -1;
+//}
 //int line_cylinder(t_cylinder cylinder, t_line line, t_vector *result)
 //{
 //	t_vector RDf;
