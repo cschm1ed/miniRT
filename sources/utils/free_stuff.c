9b1b0edbbf23dfc176d96bf4f3bc7763bf2b3d16
EikoFresh <109620716+EikoFresh@users.noreply.github.com>
2023-07-29 19:06:39 +0200

    moin

--- sources/utils/free_stuff.c
+++ sources/utils/free_stuff.c
@@ -17,6 +17,8 @@ int	free_stuff(t_data *data)
 	mlx_destroy_image(data->mlx_data.mlx, data->mlx_data.img);
 	mlx_destroy_window(data->mlx_data.mlx, data->mlx_data.win);
 	free(data->mlx_data.mlx);
+	if (data->scene->all_objs)
+		ft_lstclear(&data->scene->all_objs, free);
 	if (data->scene->light_lst)
 		ft_lstclear(&data->scene->light_lst, free);
 	if (data->scene->ambient_light)
