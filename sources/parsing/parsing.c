76c507a01d75da313b7f91b2d3af8f00f42577e8
Christian Schmiedt <120606421+cschm1ed@users.noreply.github.com>
2023-09-15 20:24:26 +0200

    removed unused variable

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -86,12 +86,10 @@ int	parsing(t_data *data)
 {
 	int		fd;
 	char	*str;
-	int i;
 
 	fd = open(data->scene_filename, O_RDONLY);
 	if (fd == -1)
 		return (perror("Error open"), FAILURE);
-	i = 1;
 	str = get_next_line(fd);
 	if (str == NULL)
 		perror("malloc failure");
